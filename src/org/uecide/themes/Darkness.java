96dd861ffae512d2f83ae27887d4fe795efc72aa
Matt Jenkins <matt@majenko.co.uk>
2015-09-15 15:36:44 +0100

    Fixed for 1.6

---
+++ src/org/uecide/themes/Darkness.java
@@ -0,0 +1,7 @@
+package org.uecide.themes;
+
+public class Darkness extends ThemeControl {
+    public static void init() {
+        loadFont("/org/uecide/fonts/whitrabt.ttf");
+    }
+}
--- src/org/uecide/themes/Nocturne.java
+++
@@ -1,7 +0,0 @@
-package org.uecide.themes;
-
-public class Nocturne extends ThemeControl {
-    public static void init() {
-        loadFont("/org/uecide/fonts/nk57-monospace-no-rg.ttf");
-    }
-}
